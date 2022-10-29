3e3d613fb7ac5b73946b9720dbd4afacd7845f7b
Daniel Król <danielkrol90@gmail.com>
2022-10-29 09:14:54 +0200

    adding components and working on chatFeed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
 
-import ChatFeed from './components/ChatFeed';
-import './App.css';
+import { ChatFeed } from "./components";
+
+import "./App.css";
 
 function App() {
   return (
-
-      <ChatEngine 
-        height="100vh"
-        projectID="2b37a1e8-f714-402e-bee6-eef8bb6a0708"
-        userName="King_Daniel"
-        userSecret="wieliczka90"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      />
-
+    <ChatEngine
+      height="100vh"
+      projectID="2b37a1e8-f714-402e-bee6-eef8bb6a0708"
+      userName="King_Daniel"
+      userSecret="wieliczka90"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
