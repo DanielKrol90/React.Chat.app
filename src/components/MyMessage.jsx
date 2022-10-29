da3c5ecf8a53e4e1a2b8d22d60c732b7a1fc870a
Daniel Król <danielkrol90@gmail.com>
2022-10-29 20:42:34 +0200

    working on MyMessage comp

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
-const MyMessage = () => {
+const MyMessage = (message) => {
+
+  
+  if(message?.attachments?.length > 0) {
+    return (
+      <img src={message.attachments[0].file}
+            alt='message-attachments'
+            className='message-image'
+            style={{float: 'right'}}      
+      />
+    )
+  }
+
+
   return (
     <div>MyMessage</div>
   )
