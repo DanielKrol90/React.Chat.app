2e972236f46ff589028a16c036a77c41beb80cb4
Daniel Król <danielkrol90@gmail.com>
2022-10-29 08:23:30 +0200

    adding first component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 function App() {
@@ -7,8 +9,9 @@ function App() {
       <ChatEngine 
         height="100vh"
         projectID="2b37a1e8-f714-402e-bee6-eef8bb6a0708"
-        userName="DarQuest"
+        userName="King_Daniel"
         userSecret="wieliczka90"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       />
 
   );
