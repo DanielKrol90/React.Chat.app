6e2bd29057ef9301680915036fd8b85d7971f037
Daniel Król <danielkrol90@gmail.com>
2022-10-30 10:57:28 +0100

    project finished

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
 
-  if(!localStorage.getItem('username')) return <LoginForm/>
+  if(!localStorage.getItem('username')) return <LoginForm />
 
   return (
     <ChatEngine
